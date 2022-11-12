0f13aa9aecb4988b941e619e85bce8a1a5caeb2c
ChickenChunk579 <87714759+ChickenChunk579@users.noreply.github.com>
2022-11-12 22:08:23 +0000

    Added Game v1.0

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,19 +1,8 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
   <div class="card">
     <Counter />
   </div>
@@ -28,17 +17,6 @@
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
   .read-the-docs {
     color: #888;
   }
--- myapp/src/lib/Counter.svelte
+++ myapp/src/lib/Counter.svelte
@@ -1,10 +1,40 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  var number = 1;
+  var level = 0;
+
+  window.onload = function() {
+    number = parseInt(window.localStorage.getItem("number"));
+    level = parseInt(window.localStorage.getItem("level"));
+  }
+
+  function increment() {
+    number += level;
+    number += Math.floor(Math.random() * 6)
+    if (number > level * 50) {
+      number = level * 50;
+    }
+    if (number % 50 == 0) {
+      level++;
+    }
+
+    window.localStorage.setItem("number", number.toString());
+    window.localStorage.setItem("level", level.toString());
+
+  }
+
+  function reset() {
+    window.localStorage.setItem("number", "0");
+    window.localStorage.setItem("level", "0");
+
+    location.reload();
   }
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<u><h1>Welcome to my amazing game!</h1></u>
+<h2>Click the button bellow to increment the number!</h2>
+<h3>If you get to some milestones, you may get a reward!</h3>
+<p>Number: {number}</p>
+<p>Level: {level}</p>
+
+<button on:click={increment}>🧠</button>
+<button on:click={reset}>Reset Game</button>
